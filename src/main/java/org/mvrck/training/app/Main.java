aae8e5a556fb4b38de508c2ef7c68fdf3b7b659b
Richard Imaoka <richard.e.imaoka@gmail.com>
2020-04-25 22:45:33 +0900

    update actor system name

--- src/main/java/org/mvrck/training/app/Main.java
+++ src/main/java/org/mvrck/training/app/Main.java
@@ -6,7 +6,7 @@ import org.mvrck.training.http.*;
 public class Main {
   public static void main(String[] args) throws Exception {
     // boot up server using the route as defined below
-    var system = ActorSystem.create(GuardianActor.create(), "user-guardian");
+    var system = ActorSystem.create(GuardianActor.create(), "MaverickTraining");
 
     System.out.println("Server online at http://localhost:8080/\nPress RETURN to stop...");
     System.in.read(); // let it run until user presses return
